a30fe65806aea1b9ad3631c39faa1d253412d768
Bess Sadler <bess@stanford.edu>
2011-08-10 14:44:37 -0700

    Accept an FTK output file as input

--- lib/ruby_ftk.rb
+++ lib/ruby_ftk.rb
@@ -1,8 +1,13 @@
 
 class RubyFtk
   
+  attr_accessor :ftk_report   # The location of the ftk xml file we're processing
+  
   def initialize(args = {})
-    
+    if args[:ftk_report]
+      raise "Can't find file #{args[:ftk_report]}" unless File.file? args[:ftk_report]
+      @ftk_report = args[:ftk_report]
+    end
   end
   
   
--- spec/lib/ruby_ftk_spec.rb
+++ spec/lib/ruby_ftk_spec.rb
@@ -11,6 +11,13 @@ describe RubyFtk do
       r = RubyFtk.new
       r.class.should eql(RubyFtk)
     end
+    
+    it "can accept an FTK XML report as input" do
+      fixture_location = File.join(File.dirname(__FILE__), "/../fixtures/Gould_FTK_Report.xml")
+      r = RubyFtk.new(:ftk_report => fixture_location)
+      r.ftk_report.should eql(fixture_location)
+    end
+    
   end
   
 end
